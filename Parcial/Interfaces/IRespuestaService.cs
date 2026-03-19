6f09b3c43e4954a53a98f4be748008437a180d27
Taverongas <taverajeronimox08@gmail.com>
2026-03-19 09:42:22 -0500

    Enviar cambios

---
+++ Parcial/Interfaces/IRespuestaService.cs
@@ -0,0 +1,7 @@
+﻿namespace Parcial.Interfaces
+{
+    public interface IRespuestaService
+    {
+        Task CrearRespuesta(int preguntaId, string contenido);
+    }
+}
---
+++ Parcial/Services/RespuestaService.cs
@@ -0,0 +1,25 @@
+﻿using Parcial.DAO;
+using Parcial.Interfaces;
+using Parcial.Models;
+
+public class RespuestaService : IRespuestaService
+{
+    private readonly ApplicationDbContext _context;
+
+    public RespuestaService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CrearRespuesta(int preguntaId, string contenido)
+    {
+        var respuesta = new Respuesta
+        {
+            PreguntaId = preguntaId,
+            Contenido = contenido
+        };
+
+        _context.Respuestas.Add(respuesta);
+        await _context.SaveChangesAsync();
+    }
+}
